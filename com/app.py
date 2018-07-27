6a83286a850d6d98e315cc1a4fd80dd05f450e5e
orhantombul <orhantombul34@gmail.com>
2018-07-27 08:57:29 +0300

    27.07.2018

--- com/app.py
+++ com/app.py
@@ -4,15 +4,16 @@ from com.Docker import Docker
 from com.DockerInfo import DockerInfo
 from com.Container import Container
 
-
 app = Flask(__name__)
+
 
 @app.route('/fill')
 def test1():
     applicationManager = ApplicationManager.getApplicationManager()
-    dockerList = [ Docker("hasan",DockerInfo("192.168.1.1","Fail"))]
-    applicationManager.setContainer("192.168.2.2",Container("192.168.2.2",dockerList))
+    dockerList = [Docker("hasan", DockerInfo("192.168.1.1", "Fail"))]
+    applicationManager.setContainer("192.168.2.2", Container("192.168.2.2", dockerList))
     return render_template("robot.html", containerMap=applicationManager.getContainers())
+
 
 @app.route('/list')
 def test():
